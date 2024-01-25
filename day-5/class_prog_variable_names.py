bf6b47262c5d7221e09a8021445d33f5bc1cca8c
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:45:03 +0530

    variables

---
+++ day-5/class_prog_variable_names.py
@@ -0,0 +1,26 @@
+
+
+# invalid ways of variable names
+number of banks = 3
+
+1account = 234234234234
+
+bank-name = "X bank"
+
+
+# valid ways 
+
+numberofbanks = 3
+
+number_of_banks = 3  # snake case  - program names, variables 
+numberOfBanks   = 3  # camel case  - variables names
+
+account1 = 23472983749823 
+account_1 = 27394872394723
+
+BANK_CODE = 9001
+PI = 3.14
+GRAV = 9.2
+
+_cvv_number = 892
+

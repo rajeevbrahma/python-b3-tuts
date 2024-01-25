9c77e2850124da87cd497dd14a61d80973739b98
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:47:34 +0530

    datatypes initialization

---
+++ day-5/define_different_datatypes.py
@@ -0,0 +1,55 @@
+# Integers 
+# About Integers
+# Integers are whole numbers
+# Integers are immutable
+
+
+a = 10
+b = 20
+
+print (a)
+print (b)
+
+# Floats
+a = 10.5
+b = 20.5
+
+print (a)
+print (b)
+
+# Complex Numbers
+a = 10 + 5j
+b = 20 + 5j
+
+print (a)
+print (b)
+
+# Strings
+a = 'hello'
+b = "world"
+c = '''hello world'''
+d = """hello world"""
+e = 'hello "world"'
+f = "hello 'world'"
+g = 'h'
+
+# About String
+# String is a sequence of characters
+# String is immutable
+# String is iterable
+# String is subscriptable
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+print (g)
+
+# Boolean
+a = True
+b = False
+
+print (a)
+print (b)

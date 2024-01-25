cefc09b6f1c2bae2d2e6596f8d0d6330a483dd1b
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:39:42 +0530

    built in funcs

---
+++ built_in_funcs.py
@@ -0,0 +1,14 @@
+""" Built in functions list """
+print() # Displays in the standard output console
+help() # helps you with the documentation of anything in python
+id()   # Return the memory location of the literal / variable given
+type() # Return the data type of the literal / variable given
+
+
+range() # Return a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+input() # which allows you to take inputs in runtime from the terminal
+len() # would give me the length of string, list
+sum( ) # returns the sum of numeric list elements
+bin()  # Return the binary representation of an integer.
+ord( ) # returns the ascii number of an alphabet or symbol
+open() # helps you open the files

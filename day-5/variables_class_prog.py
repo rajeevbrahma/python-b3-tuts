3b0a7129dfcb6563219d6048c72bf3e42f76e75c
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:47:10 +0530

    variables

---
+++ day-5/variables_class_prog.py
@@ -0,0 +1,41 @@
+a = 1
+
+b = 1.2
+
+c = 1 + 2j
+
+# d = "" / '' / ''' ''' / """ """
+
+d = "hello"
+e = 'hello'
+f = '''hello'''
+g = """hello"""
+
+# we cannot replace any single number from this below
+# number till it stays as an integer type
+h = 123123123124124
+
+
+# strings are iterable
+a = "hello world"
+#    ||||||||||| 
+#    012345678910
+
+print (a)
+# format to access a character from the iterable string is
+
+# variable_name_where_you_declared[index_number]
+
+# string slicing
+
+print (a[0],a[1],a[2])
+
+print (a[0])
+
+# kewords  - reserved words in a language which has purpose
+
+a = True
+b = False
+a = 1   
+b = 0
+

226356d0829581319f59a3174f5a1729c701913c
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:45:59 +0530

    functions intro

---
+++ day-5/class_prog_funcs.py
@@ -0,0 +1,39 @@
+
+
+# ((a+b)/(c))*d+e-(f*h)
+
+# a = 1,b=2, c=4 ...
+
+# addition op1, op2
+    # add_result = op1 + op2 
+    # return add_result
+
+
+# add_result = addition(a,b) 
+# div_result = add_result / c
+# mul_result = div_result * d
+# add_result = addition(mul_result,e)
+
+
+
+# def - keyword 
+
+# function definition
+def area_of_circle(radius):
+    # code block
+    print ("This function is to crea of circle")
+    PI = 3.14
+    print (PI * radius * radius)
+
+
+area_of_circle(4)    # - Function call
+area_of_circle(5)    # - Function call
+area_of_circle(6)    # - Function call
+
+# PI = 3.14
+# radius = 4
+# print (PI * radius * radius)
+# radius = 5
+# print (PI * radius * radius)
+# radius = 6
+# print (PI * radius * radius)

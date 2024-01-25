1b83124130374946bea3958cf141cf875dd339ed
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-25 21:47:42 +0530

    exercise

---
+++ day-5/exercise_to_declare_info.py
@@ -0,0 +1,7 @@
+"""
+    1. Declare a variable named "name" and assign a string value to it.
+    2. Declare a variable named "age" and assign a integer value to it.
+    3. Declare a variable named "salary" and assign a float value to it.
+    4. Declare a variable named "is_married" and assign a boolean value to it.
+"""
+

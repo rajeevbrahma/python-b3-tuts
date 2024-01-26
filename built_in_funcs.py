3755b9dab8ed3ea47cb769d0920658b197e31a00
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-26 09:20:46 +0530

    day-7 : len func

--- built_in_funcs.py
+++ built_in_funcs.py
@@ -1,13 +1,12 @@
 """ Built in functions list """
-print() # Displays in the standard output console
+print() # Displays in the standard output console 
 help() # helps you with the documentation of anything in python
 id()   # Return the memory location of the literal / variable given
 type() # Return the data type of the literal / variable given
-
+len() # would give me the length of string, list
 
 range() # Return a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
 input() # which allows you to take inputs in runtime from the terminal
-len() # would give me the length of string, list
 sum( ) # returns the sum of numeric list elements
 bin()  # Return the binary representation of an integer.
 ord( ) # returns the ascii number of an alphabet or symbol

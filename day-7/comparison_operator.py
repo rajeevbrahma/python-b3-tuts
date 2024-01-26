d6a96653406cb5b2af46143cb69531759e6af2e9
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-01-26 09:22:03 +0530

    day -7 : comparison operator

---
+++ day-7/comparison_operator.py
@@ -0,0 +1,24 @@
+# comparison operator
+
+# ==, !=, >, <, >=, <=
+
+# We can compare numbers (int,float), strings, lists
+
+print(10 == 10)
+print(10 != 10)
+print(10 > 10)
+print(10 < 10)
+print(10 >= 10)
+print(10 <= 10)
+
+
+# exercise 
+# take two variables a, b and initialize them with 10 and 20
+# check if the user input is greater than 100
+# check if the user input is less than 100
+# check if the user input is equal to 100
+# check if the user input is not equal to 100
+# check if the user input is greater than or equal to 100
+# check if the user input is less than or equal to 100
+# 10 with 10.0 compare the equality
+

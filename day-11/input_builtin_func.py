e882242e08060b4078b4c3077a35d827c646f4b7
rajeevbrahma <rajeevbrahma@hotmail.com>
2024-02-05 18:34:53 +0530

    input builtin func prog

---
+++ day-11/input_builtin_func.py
@@ -0,0 +1,10 @@
+# input built-in function 
+
+a = int(input("Enter numbers from 0 to 9 - "))   # by default the input provided will be of str datatype
+b = int(input("Enter numbers from 0 to 9 - "))
+
+print (a+b)
+
+# exercise 
+# walmart example
+
